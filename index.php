56192eb112db484acc6a4a1676cb7c2a82f79d1b
Webme Studio <webmestudio@gmail.com>
2020-12-05 14:12:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -121,7 +121,7 @@ function getInstagramReaction($username, $param) {
     curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36');
     
     $data = curl_exec($ch);
-    curl_close($ch);
+    //curl_close($ch);
 	
 	echo $data;
 	
