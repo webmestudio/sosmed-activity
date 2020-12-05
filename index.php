4ee85ffb54a4e19e9a331584ea94a4845938222e
Webme Studio <webmestudio@gmail.com>
2020-12-05 13:00:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -104,7 +104,7 @@ function getYoutubeSubscriber($youtube_channel_id) {
 
 //returns a big old hunk of JSON from a non-private IG account page.
 function scrape_insta($username) {
-	$insta_source = file_get_contents('http://instagram.com/'.$username);
+	$insta_source = file_get_contents('http://instagram.com/'.$username.'?__a=1');
 	$shards = explode('window._sharedData = ', $insta_source);
 	$insta_json = explode(';</script>', $shards[1]); 
 	$insta_array = json_decode($insta_json[0], TRUE);
