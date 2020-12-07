d51d8a45e5dc4925974e70c17a5e1bb51b30e933
Webme Studio <webmestudio@gmail.com>
2020-12-07 11:44:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 set_time_limit(0);
-//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 
 /**
  * @packcage [sosmedstats]
