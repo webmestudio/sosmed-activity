54c6791063d7dfcab5f59c13d44e0f43460ea6f9
Webme Studio <webmestudio@gmail.com>
2020-12-16 09:52:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -116,7 +116,6 @@ function getInstagramReaction($username, $param) {
 	login_inst();
 	sleep(5);
 	
-	/*
 	$data = curl_inst('https://www.instagram.com/'. $username);
     
     // Load HTML to DOM Object
@@ -151,7 +150,6 @@ function getInstagramReaction($username, $param) {
     }
 
     echo json_encode($output); 
-	*/
 }
 
 function getFBFansPageReaction($username, $param) {    
